2f4d5f7b61e48936db5a134198af127357687bf5
Håkon Kiær <haakonkiaer@gmail.com>
2025-12-13 06:57:11 +0100

    discovery document

--- django-helseid/helseid/views.py
+++ django-helseid/helseid/views.py
@@ -7,32 +7,31 @@ from django.http import HttpResponse
 from django.conf import settings
 
 
-
-
-
 def home(request):
-    user = request.session.get('user')
+    user = request.session.get("user")
     if user:
         user = json.dumps(user)
-    return render(request, 'helseid/home.html', context={'user': user})
+    return render(request, "helseid/home.html", context={"user": user})
 
 
 def login(request):
     # Bygg redirect URLen til din callback view
-    django_redirect_uri = request.build_absolute_uri(reverse('auth')) 
+    django_redirect_uri = request.build_absolute_uri(reverse("auth"))
 
-
-    oauth2client = OAuth2Client(
-        token_endpoint="https://url.to.the/token_endpoint",
+    oauth2client = OAuth2Client.from_discovery_document(
+        {
+            "issuer": "https://helseid-sts.test.nhn.no",
+            "token_endpoint": "https://helseid-sts.test.nhn.no/connect/token",
+        },
         client_id=settings.HELSEID_CLIENT_ID,
         client_secret=settings.HELSEID_CLIENT_SECRET,
     )
-    print(django_redirect_uri)
+    print(oauth2client)
     return HttpResponse("yay")
 
 
 def auth(request):
 
     # Do auth stuff
-    
-    return redirect('home')
+
+    return redirect("home")
